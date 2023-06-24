b5735550ae5dabd817eddfa60e9d92179e68a802
BhubonCosta <bhuboncosta94@gmail.com>
2023-06-24 14:31:04 +0600

    email_slicer

---
+++ email_slicer.py
@@ -0,0 +1,12 @@
+def main():
+    print(" Welcome to the email slicer ")
+    print("")
+
+    email_input = input("Input your email address: ")
+
+    (username, domain) = email_input.split("@")
+    (domain, extension) = domain.split(".")
+
+    print(username)
+    print(domain)
+    print(extension)
